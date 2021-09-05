1929d9068e6078d7f939c018211f3e06d2a4dcd4
kirik <kirik550088@gmail.com>
2021-09-05 19:25:16 +0300

    First task desription

--- src/ru/mireea/prac/Book.java
+++ src/ru/mireea/prac/Book.java
@@ -31,6 +31,7 @@ public class Book {
     }
 
     public int getPrice(int price_book) {
+
         return price_book;
     }
 
--- src/ru/mireea/prac/TestBook.java
+++ src/ru/mireea/prac/TestBook.java
@@ -1,11 +1,14 @@
 package ru.mireea.prac;
 
 import java.lang.*;
+import java.util.Scanner;
 
 public class TestBook {
     public static void main(String args[]) {
         Book b1 = new Book("Harry Potter", 345);
         Book b2 = new Book("Martin Eden");
         System.out.println(b1.getName());
+        System.out.println(b1.getPrice(21));
+        Scanner
     }
 }
