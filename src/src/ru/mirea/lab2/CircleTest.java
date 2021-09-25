1d34ec15a411ffff5e0d5ee6c13c740bb616bb5c
kirik <kirik550088@gmail.com>
2021-09-25 17:34:38 +0300

    Full dev project

---
+++ src/src/ru/mirea/lab2/CircleTest.java
@@ -0,0 +1,11 @@
+package ru.mirea.lab2;
+
+public class CircleTest {
+    public static void main(String[] args) {
+        Cirle obj = new Cirle();
+        obj.setRadius(21);
+        System.out.println(obj.Perimeter());
+        System.out.println(obj.Square());
+
+    }
+}
