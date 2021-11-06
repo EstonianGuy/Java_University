14eb7b093ff40b240a938f2fdeb11e6677e00af0
Kirill <74711970+EstonianGuy@users.noreply.github.com>
2021-11-06 13:49:31 +0300

    Create Student.java

---
+++ Student.java
@@ -0,0 +1,34 @@
+package com.sixprac;
+
+
+public class Student implements Comparable<Student> {
+    private int idNumber;
+    private String name;
+    private double GPA;
+
+    public Student() {
+        this.idNumber = 0;
+        this.name = "";
+        this.GPA = 0;
+    }
+
+    public Student(int idNumber, String name, double gpa) {
+        this.idNumber = idNumber;
+        this.name = name;
+        this.GPA = gpa;
+    }
+
+    @Override
+    public String toString() {
+        return "Student { idNumber: \"" + idNumber + "\", name: \"" + name + "\", gpa: \"" + GPA + "\" }";
+    }
+
+    public double getGPA() {
+        return GPA;
+    }
+
+    @Override
+    public int compareTo(Student other) {
+        return (int) ((other.getGPA() - this.getGPA()) * 100);
+    }
+}
