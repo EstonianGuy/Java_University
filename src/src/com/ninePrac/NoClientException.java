3c54d0d77cea8c9acd8ac3fcaaee576b6b3d13d0
Kirill <74711970+EstonianGuy@users.noreply.github.com>
2021-11-17 10:57:55 +0300

    Delete src/src directory

--- src/src/com/ninePrac/NoClientException.java
+++
@@ -1,7 +0,0 @@
-package com.ninePrac;
-
-public class NoClientException extends MarketException{
-    public NoClientException(String message){
-        super("Error"+message);
-    }
-}
--- src/src/com/ninePrac/NoNumberException.java
+++
@@ -1,9 +0,0 @@
-package com.ninePrac;
-
-public class NoNumberException extends MarketException{
-    public NoNumberException(int message){
-        super("Your number doesn't exist"+message);
-
-    }
-
-}
